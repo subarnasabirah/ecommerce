d395097a174a8c63d2686147cd964e7e536a23ce
subarna <subarna769@gmail.com>
2020-02-29 00:43:20 +0600

    migration table create start

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name',100);
             $table->string('email')->unique();
+            $table->string('phone',15)->unique();
+            $table->string('image')->nullable();
+            $table->enum('gender',['Male','Female'])->default('0');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
